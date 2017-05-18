3d7a59082f09741a6428e4446c29efd276dc0cc5
dg <251314718@qq.com>
2017-05-18 22:58:47 +0800

    增加基本文件

--- src/test/java/com/github/denggeng/learning/core/TestCrack.java
+++ src/test/java/com/github/denggeng/learning/core/TestCrack.java
@@ -46,52 +46,32 @@ public class TestCrack {
     }
 
     @Test
-    public void play() {
-        String bip = "D:\\tmp\\baicizan\\mp3\\zp_1828_11_0_12_161221133010.mp3";
+    public void testMulti() {
+        String mp31 = "D:\\tmp\\baicizan\\mp3\\zp_1828_11_0_12_161221133010.mp3";
+        String mp32 = "D:\\tmp\\baicizan\\mp3\\zp_1828_11_0_12_161221133010.mp3";
+        read(mp31);
+        read(mp32);
+        read("D:\\tmp\\baicizan\\mp3\\zp_188_11_0_10_161221131838.mp3");
+        read("D:\\tmp\\baicizan\\mp3\\zp_295_11_0_10_161221131934.mp3");
+    }
+
+    public void read(String bip) {
         Mp3File mp3file = null;
         try {
             mp3file = new Mp3File(bip);
-            System.out.println("Length of this mp3 is: " + mp3file.getLengthInSeconds() + " seconds");
-            System.out.println("Bitrate: " + mp3file.getBitrate() + " kbps " + (mp3file.isVbr() ? "(VBR)" : "(CBR)"));
-            System.out.println("Sample rate: " + mp3file.getSampleRate() + " Hz");
-            System.out.println("Has ID3v1 tag?: " + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
-            System.out.println("Has ID3v2 tag?: " + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
-            System.out.println("Has custom tag?: " + (mp3file.hasCustomTag() ? "YES" : "NO"));
+            //System.out.println("Length of this mp3 is: " + mp3file.getLengthInSeconds() + " seconds");
+            //System.out.println("Bitrate: " + mp3file.getBitrate() + " kbps " + (mp3file.isVbr() ? "(VBR)" : "(CBR)"));
+            //System.out.println("Sample rate: " + mp3file.getSampleRate() + " Hz");
+            //System.out.println("Has ID3v1 tag?: " + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
+            //System.out.println("Has ID3v2 tag?: " + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
+            //System.out.println("Has custom tag?: " + (mp3file.hasCustomTag() ? "YES" : "NO"));
+            String cuntomTag = new String(mp3file.getCustomTag(), "UTF-8");
+            String[] cuntomTags = cuntomTag.split("\\n");
 
-            if (mp3file.hasId3v1Tag()) {
-                ID3v1 id3v1Tag = mp3file.getId3v1Tag();
-                System.out.println("Track: " + id3v1Tag.getTrack());
-                System.out.println("Artist: " + id3v1Tag.getArtist());
-                System.out.println("Title: " + id3v1Tag.getTitle());
-                System.out.println("Album: " + id3v1Tag.getAlbum());
-                System.out.println("Year: " + id3v1Tag.getYear());
-                System.out.println("Genre: " + id3v1Tag.getGenre() + " (" + id3v1Tag.getGenreDescription() + ")");
-                System.out.println("Comment: " + id3v1Tag.getComment());
-            }
+            System.out.println("Custom tag: " + cuntomTag);
+            System.out.println("Custom tag0: " + cuntomTags[0] + "#" + cuntomTags[1] + "#" + cuntomTags[2] + "#" + cuntomTags[3]);
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
-            if (mp3file.hasId3v2Tag()) {
-                ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-                System.out.println("Track: " + id3v2Tag.getTrack());
-                System.out.println("Artist: " + id3v2Tag.getArtist());
-                System.out.println("Title: " + id3v2Tag.getTitle());
-                System.out.println("Album: " + id3v2Tag.getAlbum());
-                System.out.println("Year: " + id3v2Tag.getYear());
-                System.out.println("Genre: " + id3v2Tag.getGenre() + " (" + id3v2Tag.getGenreDescription() + ")");
-                System.out.println("Comment: " + id3v2Tag.getComment());
-                System.out.println("Lyrics: " + id3v2Tag.getLyrics());
-                System.out.println("Composer: " + id3v2Tag.getComposer());
-                System.out.println("Publisher: " + id3v2Tag.getPublisher());
-                System.out.println("Original artist: " + id3v2Tag.getOriginalArtist());
-                System.out.println("Album artist: " + id3v2Tag.getAlbumArtist());
-                System.out.println("Copyright: " + id3v2Tag.getCopyright());
-                System.out.println("URL: " + id3v2Tag.getUrl());
-                System.out.println("Encoder: " + id3v2Tag.getEncoder());
-                byte[] albumImageData = id3v2Tag.getAlbumImage();
-                if (albumImageData != null) {
-                    System.out.println("Have album image data, length: " + albumImageData.length + " bytes");
-                    System.out.println("Album image mime type: " + id3v2Tag.getAlbumImageMimeType());
-                }
-            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (UnsupportedTagException e) {
