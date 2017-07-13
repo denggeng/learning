9086d214b7f83f665e5ff0d052618f6464b8b604
dg <251314718@qq.com>
2017-07-13 23:14:55 +0800

    add db control

--- src/test/java/com/github/denggeng/learning/core/TestCrack.java
+++ src/test/java/com/github/denggeng/learning/core/TestCrack.java
@@ -28,4 +28,53 @@ public class TestCrack {
             }
         }
     }
+
+    private void extraSingle(File zpkFile, String preZpkFolder, String prefixExtraFolder) {
+        String fullApkPath = zpkFile.getPath();
+        String shortZpkPath = fullApkPath.replace(preZpkFolder, "");
+        String fullExtractPath = prefixExtraFolder + shortZpkPath.replace(".zpk", "");
+        System.out.println("shortZpkPath:" + shortZpkPath);
+        System.out.println("fullExtractPath:" + fullExtractPath);
+        //Path extraPath = Paths.get(fullExtractPath);
+        File extraFolder = new File(fullExtractPath);
+
+        if (!extraFolder.exists()) {
+            boolean mkdirs = extraFolder.mkdirs();
+        }
+        ZPackage zPackage = null;
+        try {
+            zPackage = new ZPackage(fullApkPath, 1);
+            ZPacks.unpack(zPackage, extraFolder.getPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testExtractOne() {
+        File file = new File("D:\\study\\words\\1\\0\\zp_130_1_0_14_170326020049.zpk");
+        extraSingle(file, "D:\\study\\words", "D:\\study\\extract");
+    }
+
+    private void treeExtract(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File innerFile : files) {
+                    treeExtract(innerFile);
+                }
+            }
+
+        } else {
+            if (file.getPath().endsWith(".zpk")) {
+                extraSingle(file, "D:\\study\\words", "D:\\study\\extract");
+            }
+        }
+    }
+
+    @Test
+    public void testExtractAll() {
+        File wordsDir = new File("D:\\study\\words");
+        treeExtract(wordsDir);
+    }
 }
